a5938835b7b671798e3803dc72a7acc3df70ca32
bertlomewos <160644577+bertlomewos@users.noreply.github.com>
2025-05-07 14:22:00 +0300

    added the login fragment

---
+++ app/src/main/java/com/example/gocip/Login.java
@@ -0,0 +1,29 @@
+package com.example.gocip;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Login#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Login extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
