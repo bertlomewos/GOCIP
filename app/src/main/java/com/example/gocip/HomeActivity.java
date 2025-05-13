8951b965d6e9671f0a25774fff5ceeb81a0bc80e
bertlomewos <160644577+bertlomewos@users.noreply.github.com>
2025-05-13 14:24:25 +0300

    Making the home Activity

---
+++ app/src/main/java/com/example/gocip/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.gocip;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gocip.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/gocip/HomeFragment.java
@@ -0,0 +1,27 @@
+package com.example.gocip;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gocip.databinding.FragmentHomeBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
